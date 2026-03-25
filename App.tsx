bd4e2d1006efce909a1a756fd59f14253dc78f2c
Z7F-KING <akdnss37@gmail.com>
2026-03-25 11:29:24 +0300

    Create App.tsx

---
+++ App.tsx
@@ -0,0 +1,47 @@
+import { Toaster } from "@/components/ui/sonner";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/NotFound";
+import { Route, Switch } from "wouter";
+import ErrorBoundary from "./components/ErrorBoundary";
+import { ThemeProvider } from "./contexts/ThemeContext";
+import Home from "./pages/Home";
+import VerificationPage from "./pages/VerificationPage";
+import MainPage from "./pages/MainPage";
+import ErrorPage from "./pages/ErrorPage";
+
+function Router() {
+  return (
+    <Switch>
+      <Route path={"/"} component={VerificationPage} />
+      <Route path={"/verification"} component={VerificationPage} />
+      <Route path={"/main"} component={MainPage} />
+      <Route path={"/error"} component={ErrorPage} />
+      <Route path={"/home"} component={Home} />
+      <Route path={"/404"} component={NotFound} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+// NOTE: About Theme
+// - First choose a default theme according to your design style (dark or light bg), than change color palette in index.css
+//   to keep consistent foreground/background color across components
+// - If you want to make theme switchable, pass `switchable` ThemeProvider and use `useTheme` hook
+
+function App() {
+  return (
+    <ErrorBoundary>
+      <ThemeProvider
+        defaultTheme="light"
+        // switchable
+      >
+        <TooltipProvider>
+          <Toaster />
+          <Router />
+        </TooltipProvider>
+      </ThemeProvider>
+    </ErrorBoundary>
+  );
+}
+
+export default App;
